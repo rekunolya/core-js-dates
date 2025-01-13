bb7c5dd14f17892a2eec71d827e997eadbb8808e
rekunolya <rekun.olya@gmail.com>
2025-01-14 01:02:37 +0300

    feat: add a solution to task isDateInPeriod

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -152,8 +152,14 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const start = new Date(period.start).getTime();
+  const end = new Date(period.end).getTime();
+  const day = new Date(date).getTime();
+  if (day >= start && day <= end) {
+    return true;
+  }
+  return false;
 }
 
 /**
